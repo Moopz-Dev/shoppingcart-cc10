19af244d6a55c08a85f25327a24d7c7148602df2
Moopz-Dev <pongzthor@gmail.com>
2021-12-15 12:42:15 +0700

    updated_basket

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -13,8 +13,29 @@ function Basket() {
 				<div className="col-2 text-right">3 x $5000.00</div>
 			</div>
 			<div className="row">
-				<div>Product name</div>
-				<div></div>
+				<div>Product Name</div>
+				<div>
+					<button className="add">+</button>
+					<button className="remove">-</button>
+				</div>
+				<div className="col-2 text-right">3 x $5000.00</div>
+			</div>
+			<hr />
+			<div className="row">
+				<div className="col-2">Items Price</div>
+				<div className="col-1 text-right">$10000.00</div>
+			</div>
+			<div className="row">
+				<div className="col-2">Tax</div>
+				<div className="col-1 text-right">$700.00</div>
+			</div>
+			<div className="row">
+				<div className="col-2">Shipping</div>
+				<div className="col-1 text-right">$20.00</div>
+			</div>
+			<div className="row text-bold">
+				<div className="col-2">Total</div>
+				<div className="col-1 text-right">$17200.00</div>
 			</div>
 		</aside>
 	);
